e3cc57ce1a034d4e9f366330be705554e0af19ca
ntiwari-z <nikhil@zentrumhub.com>
2024-01-29 14:32:36 +0530

    changes

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -11,9 +11,17 @@ namespace Tests
         public BaseTest(ITestOutputHelper output) : base(output)
         {
             var correlationId = Guid.NewGuid().ToString();
+
             CustomTestContext.Init(correlationId, TestConfiguration.AppSetting);
             AppSettings = TestConfiguration.AppSetting;
             _output = output;
+
+            _output.WriteLine("Base Test");
+            _output.WriteLine(TestConfiguration.AppSetting.HotelApiBaseUrl);
+            _output.WriteLine(TestConfiguration.AppSetting.ChannelId);
+            _output.WriteLine(TestConfiguration.AppSetting.AccountId);
+            _output.WriteLine(TestConfiguration.AppSetting.LocationBaseUrl);
+            _output.WriteLine(correlationId);
         }
 
         public override void Dispose()
